589e179093e4297deabd85afec5c0cb3d6f47dec
tephL <rhezstephen06@gmail.com>
2026-08-18 12:55:42 +0800

    feat(auth): usable isUserLoggedIn middleware to protect routes

--- src/app.mjs
+++ src/app.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import authRoute from '#/routes/authRoutes.mjs';
+import peopleRoute from '#/routes/peopleRoutes.mjs';
 
 export const DEV = process.env.DEV;
 const PORT = DEV ? 3000 : process.env.PORT;
@@ -9,6 +10,7 @@ const PORT = DEV ? 3000 : process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoute);
+app.use('/api/people', peopleRoute);
 
 app.listen(PORT, () => {
     console.log(`running at port: ${PORT}`);
