6b0a5de22b615f521f08e4ee752f6dd769e4f1f4
tephL <rhezstephen06@gmail.com>
2026-08-18 10:07:27 +0800

    export DEV status to sql connection

--- src/app.mjs
+++ src/app.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const DEV = process.env.DEV;
+export const DEV = process.env.DEV;
 const PORT = DEV ? 3000 : process.env.PORT;
 
 const app = express();
