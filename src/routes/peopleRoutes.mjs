78bb3b077770808d30be8c6f64c0a387ce547bcd
tephL <rhezstephen06@gmail.com>
2026-08-18 13:23:56 +0800

    feat(people): validators for personal details

--- src/routes/peopleRoutes.mjs
+++ src/routes/peopleRoutes.mjs
@@ -2,9 +2,9 @@ import { Router } from "express";
 import * as helperMid from '#/middlewares/helper-mid.mjs';
 
 const router = Router();
+router.use(helperMid.isUserLoggedIn);
 
-router.get('/',
-    helperMid.isUserLoggedIn, 
+router.post('/',
     (req, res) => {
         return res.sendStatus(200);
     }
