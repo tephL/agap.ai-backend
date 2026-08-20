f87a5d978123e331a7118fe6ce52036ca28e5dcb
Sneaky <justinedimla000@gmail.com>
2026-08-20 15:43:45 +0800

    Working with family frontend

--- src/services/db.mjs
+++ src/services/db.mjs
@@ -1,12 +1,15 @@
 import { Pool } from "pg";
 import { DEV } from '#/config/env.mjs';
 
+const isLocalHost = ['localhost', '127.0.0.1'].includes(process.env.DB_HOST);
+
 export const pool = new Pool({
     user: process.env.DB_USERNAME, 
     host: process.env.DB_HOST, 
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME, 
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: isLocalHost ? false : { rejectUnauthorized: false }
 });
 
 export function query(text, values){
